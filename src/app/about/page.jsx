251b676b5d26ad05d7b6c6d27144748dd2b6c766
googoodool <47731412+googoodool@users.noreply.github.com>
2023-11-22 13:53:59 +0700

    add bbout page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,50 @@
+function AboutPage() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+
+      <p>
+        A website (also written as a web site) is a collection of web pages and
+        related content that is identified by a common domain name and published
+        on at least one web server. Websites are typically dedicated to a
+        particular topic or purpose, such as news, education, commerce,
+        entertainment or social networking.
+      </p>
+      <p>
+        Hyperlinking between web pages guides the navigation of the site, which
+        often starts with a home page. As of May 2023, the top 5 most visited
+        websites are Google Search, YouTube, Facebook, Twitter, and Instagram.
+        All publicly accessible websites collectively constitute the World Wide
+        Web. There are also private websites that can only be accessed on a
+        private network, such as a company's internal website for its employees.
+        Users can access websites on a range of devices, including desktops,
+        laptops, tablets, and smartphones. The app used on these devices is
+        called a web browser.
+      </p>
+      <p>
+        While "web site" was the original spelling (sometimes capitalized "Web
+        site", since "Web" is a proper noun when referring to the World Wide
+        Web), this variant has become rarely used, and "website" has become the
+        standard spelling. All major style guides, such as The Chicago Manual of
+        Style[4] and the AP Stylebook,[5] have reflected this change. In
+        February 2009, Netcraft, an Internet monitoring company that has tracked
+        Web growth since 1995, reported that there were 215,675,903 websites
+        with domain names and content on them in 2009, compared to just 19,732
+        websites in August 1995.[6] After reaching 1 billion websites in
+        September 2014, a milestone confirmed by Netcraft in its October 2014
+        Web Server Survey and that Internet Live Stats was the first to
+        announce—as attested by this tweet from the inventor of the World Wide
+        Web himself, Tim Berners-Lee—the number of websites in the world have
+        subsequently declined, reverting to a level below 1 billion. This is due
+        to the monthly fluctuations in the count of inactive websites. The
+        number of websites continued growing to over 1 billion by March 2016 and
+        has continued growing since.[7] Netcraft Web Server Survey in January
+        2020 reported that there are 1,295,973,827 websites and in April 2021
+        reported that there are 1,212,139,815 sites across 10,939,637 web-facing
+        computers, and 264,469,666 unique domains.[8] An estimated 85 percent of
+        all websites are inactive.[9]
+      </p>
+    </div>
+  );
+}
+export default AboutPage;
